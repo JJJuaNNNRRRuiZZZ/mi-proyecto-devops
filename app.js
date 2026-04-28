eb468df3c0e0fad4ae862bf5df0a57a4a76bf1dc
Juan Ruiz <11.juancamiloruiz@gmail.com>
2026-04-28 12:49:35 -0500

    Implementando mi primer pipeline de CD completo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>¡Hola DevOps! Volví más fuerte.</h1>');
+    res.send('<h1> ¡Hola DevOps, Este es mi primer Despliegue Continuo (CD) automatizado.</h1>');
 });
 
 app.listen(PORT, () => console.log('Servidor corriendo en puerto 3000'));
